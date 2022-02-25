8abe5631aa15709803bf2c59902ba68aed24ea04
theopan8 <tpan729@gmail.com>
2022-02-24 23:15:35 -0800

    Create grid board

---
+++ src/components/GridBoard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import GridSquare from './GridSquare'
+
+
+export default function GridBoard(props) {
+    const grid = []
+    for (let row = 0; row < 18; row ++) {
+        grid.push([])
+        for (let col = 0; col < 10; col ++) {
+            grid[row].push(<GridSquare key={`${col}${row}`} color="1" />)
+        }
+    }
+
+    return (
+        <div className='grid-board'>
+            {grid}
+        </div>
+    )
+}
