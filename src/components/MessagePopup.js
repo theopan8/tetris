33f3b115351865e9fbe4d19b95116ed0267ae9f9
theopan8 <tpan729@gmail.com>
2022-02-25 10:54:46 -0800

    Add init message popup

---
+++ src/components/MessagePopup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MessagePopup(props) {
+  return (
+    <div className='message-popup'>
+      <h1>Message Title</h1>
+      <p>Message info...</p>
+    </div>
+  )
+}
