bbbb3e3df6b8920120fea20c32b0e56ee74f1d35
theopan8 <tpan729@gmail.com>
2022-03-08 00:03:47 -0800

    Implement restart

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -75,7 +75,7 @@ const gameReducer = (state = defaultState(), action) => {
   
         case RESTART:
   
-            return state
+            return defaultState()
   
         default:
             return state
